f6de60b526385d0d7e2e5448e242c24265c50a92
Саша Легезин <1sashasam@rambler.ru>
2022-09-01 19:56:35 +0300

    sdfsaf

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from config import settings
-
+import os
 bot = commands.Bot(command_prefix=settings['prefix'])
 bot.load_extension('DiscordBot.bot')
-bot.run(settings['token'])
+bot.run(os.environ["Token"])
